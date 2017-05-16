1c527343b2072d1d93dfda5e2df11411e3139a6d
Jess <kohrVid@zoho.com>
2017-05-16 22:30:28 +0100

    ruby hacknight

--- lib/pr2slack/bot.rb
+++ lib/pr2slack/bot.rb
@@ -4,9 +4,10 @@ require "pry"
 module Pr2slack
   class Bot < SlackRubyBot::Bot
 
-    scan(//) do |client, data, match|
-      called = true
-      while 0 < 1
+    scan(/.*/) do |client, data, match|
+          puts "hello"
+      #called = true
+      #while 0 < 1
       repo_owner = Pr2slack.configuration.REPO_OWNER
       repo_name = Pr2slack.configuration.REPO_NAME
       github_auth_token = Pr2slack.configuration.GITHUB_AUTH_TOKEN
@@ -15,11 +16,15 @@ module Pr2slack
       #http = Net::HTTP.new(uri.host, uri.port).start
       #req = Net::HTTP::Get.new(uri.request_uri, header)
       #res = http.request(req)
-      res = Net::HTTP.get(uri)
+      res = Net::HTTP.get_response(uri)
       @last_pr = {}
-      if !res.empty?
-        pr = JSON.parse(res).sort{|b| b["updated_at"] > b["updated_at"]}.last
+      #if res.
+      if !res.body.empty?
+        pull_request = JSON.parse(res.body).sort_by do |pr| 
+          pr["updated_at"]
+        end.last
         message = format_message(pr)
+          client.say(text: "hello", channel: data.channel)
 
         if pr == @last_pr
           puts "mew"
@@ -28,7 +33,7 @@ module Pr2slack
           @last_pr = pr
         end
       end
-      end
+      #end
     end
     
     def self.format_message(pr)
